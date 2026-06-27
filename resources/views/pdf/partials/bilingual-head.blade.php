c13b09e2d5005ff94389a1de8cbba9c8a1f313ed
MuarLab <mustapha.2003.arab@gmail.com>
2026-06-27 12:16:38 +0100

    Remove Scribe documentation

--- resources/views/pdf/partials/bilingual-head.blade.php
+++
@@ -1,6 +0,0 @@
-<table width="100%" style="border-collapse: collapse;">
-    <tr>
-        <td style="font-size: 7pt; font-weight: bold; text-align: left; text-transform: uppercase;">{{ $fr }}</td>
-        <td class="ar-label" style="width: 42%;">{{ $ar }}</td>
-    </tr>
-</table>
--- resources/views/pdf/partials/bilingual-label.blade.php
+++
@@ -1,6 +0,0 @@
-<table width="100%" style="border-collapse: collapse;">
-    <tr>
-        <td class="fr-label">{{ $fr }}</td>
-        <td class="ar-label">{{ $ar }}</td>
-    </tr>
-</table>
