bde8116444b74ce16317339b5c5e64853bc77b7f
ayoubelmssellek <mssallak8@gmail.com>
2026-07-02 14:27:39 +0200

    add update

---
+++ resources/views/pdf/partials/bilingual-head.blade.php
@@ -0,0 +1,6 @@
+<table width="100%" style="border-collapse: collapse;">
+    <tr>
+        <td style="font-size: 7pt; font-weight: bold; text-align: left; text-transform: uppercase;">{{ $fr }}</td>
+        <td class="ar-label" style="width: 42%;">{{ $ar }}</td>
+    </tr>
+</table>
